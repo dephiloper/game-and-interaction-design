a95117c559001d743e2c0d633a37a7648efe165a
Philipp <philippboensch@gmail.com>
2019-05-18 12:17:15 +0200

    player movement now possible w/ controller
    added more planets

--- gravityworrior/Planet.gd
+++ gravityworrior/Planet.gd
@@ -3,12 +3,13 @@ extends Node2D
 class_name Planet
 
 var gravity: float = 0
-var radius: float = 0
 
+var _radius: float = 0
 var _planet_points: PoolVector2Array
 var _num_points: int = 0
 var _offset: float = 0
 var _initialized: bool = false
+const GRAVITY_MULTIPLIER: float = 2000.0
 
 func _init() -> void:
 	generate(self.position)
@@ -25,7 +26,7 @@ func generate(center: Vector2, radius: float = rand_range(32, 64),
 	num_points: int = int(rand_range(12,20)), 
 	offset: float = rand_range(2,4)) -> void:
 	self.position = center
-	self.radius = radius
+	self._radius = radius
 	self._num_points = num_points
 	self._offset = offset
 	self._planet_points = _calculate_planet_points()
@@ -36,7 +37,7 @@ func _calculate_planet_points() -> PoolVector2Array:
 	var angle: float = 2 * PI / self._num_points
 	var points: PoolVector2Array = []
 	for i in self._num_points:
-		var r = rand_range(self.radius-self._offset, self.radius+self._offset)
+		var r = rand_range(self._radius-self._offset, self._radius+self._offset)
 		self.gravity += r
 		var x: float = r * cos(i * angle)
 		var y: float = r * sin(i * angle)
@@ -44,7 +45,7 @@ func _calculate_planet_points() -> PoolVector2Array:
 	points.append(points[0])
 	
 	self.gravity /= self._num_points
-	self.gravity *= 300
+	self.gravity *= GRAVITY_MULTIPLIER
 	return points
 	
 func _create_collision_shape():
--- gravityworrior/src/Player.gd
+++ gravityworrior/src/Player.gd
@@ -1,50 +1,61 @@
 extends KinematicBody2D
 
+const MOVEMENT_SPEED: int = 10
+const JUMP_SPEED_MULTIPLIER: float = 3.0
+
+const ON_PLANET_DRAG: float = 0.9
+const ON_PLANET_SPEED_MULTIPLIER: float = 2.0
+const OFF_PLANET_DRAG: float = 0.98
+const OFF_PLANET_MAX_VELOCITY: int = 300
+
 var velocity = Vector2()
 var closest_planet: Planet
 var is_on_planet: bool = false
-const MOVEMENT_SPEED: int = 20
-const MAX_SPEED: int = 200
+var is_boosting: bool = false
 
 class_name Player
 
 func _physics_process(delta: float) -> void:
-	velocity += _calculate_player_movement()
-	
 	if is_on_planet == true:
-		velocity *= 0.9
-		var x = closest_planet.position - position
-		velocity = move_and_slide_with_snap(velocity, x, -x)
-		velocity = velocity.slide(x.normalized())
-		velocity = velocity.slide(-x.normalized())
+		velocity += _calculate_player_movement()
+		velocity *= ON_PLANET_DRAG
+		var diff: Vector2 = closest_planet.position - position
+		velocity = move_and_slide_with_snap(velocity, diff, -diff)
+		# velocity = velocity.slide(diff.normalized())
+		# velocity = velocity.slide(-diff.normalized())
 	else:
-		velocity *= 0.99
+		velocity += _calculate_player_movement()
+		velocity *= OFF_PLANET_DRAG
 		var collision = move_and_collide(velocity * delta)
 		if collision:
 			is_on_planet = true
 		else:
 			velocity += _calculate_gravitational_pull()
+			
+		var max_velocity: float = OFF_PLANET_MAX_VELOCITY
+		if is_boosting:
+			max_velocity *= JUMP_SPEED_MULTIPLIER
+		velocity = velocity.clamped(max_velocity)
 	
 func _calculate_player_movement() -> Vector2:
-	var speed = Vector2()
-	if Input.is_action_pressed("ui_left"):
-		speed.x -= MOVEMENT_SPEED
-	if Input.is_action_pressed("ui_right"):
-		speed.x += MOVEMENT_SPEED
-	if Input.is_action_pressed("ui_up"):
-		speed.y -= MOVEMENT_SPEED
-	if Input.is_action_pressed("ui_down"):
-		speed.y += MOVEMENT_SPEED
-	speed.normalized()
+	var horizontal: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var vertical: float = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	is_boosting = false
+	
+	var movement_speed: float = MOVEMENT_SPEED
+	if is_on_planet:
+		movement_speed *= ON_PLANET_SPEED_MULTIPLIER
+		
+	var speed: Vector2 = Vector2(horizontal, vertical).normalized() * movement_speed
 	
 	if Input.is_action_pressed("ui_accept"):
-		print("jump")
 		is_on_planet = false
-		speed *= 2
+		is_boosting = true
+		speed *= JUMP_SPEED_MULTIPLIER
 	return speed
 		
 func _calculate_gravitational_pull() -> Vector2:
-	var pull = Vector2()
+	var pull: Vector2 = Vector2()
 	var closest_distance: float = INF
 	for planet in GameManager.planets:
 		var distance: float = position.distance_squared_to(planet.position)
