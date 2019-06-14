4433bbcd3043cdf07c0ed2671cdd75338cd2a41f
Bluemi <bruno.schilling@protonmail.ch>
2019-06-14 13:23:04 +0200

    added lurking

--- gravityworrior/src/EnemyAssassin.gd
+++ gravityworrior/src/EnemyAssassin.gd
@@ -1,29 +1,56 @@
 extends KinematicBody2D
 
 const SPEED: int = 10
-const ATTACK_SPEED: float = 3.0
+const ATTACK_SPEED: float = 450.0
 const DRAG: float = 0.92
+const GO_INTO_PLANET_SPEED = 50
+
 const SQUARED_ATTACK_RANGE: int = 20000
+const SQUARED_PLANET_DISTANCE: int = 15000
 const ATTACK_CHANNEL_TIME: float = 0.5
-const ATTACK_TIME: float = 2.0 / ATTACK_SPEED
+const ATTACK_TIME: float = 2.0 / 3.0
+const GO_INTO_PLANET_TIME = 0.7
+const LURK_ON_PLANET_TARGET_POINT_DIFF: int = 100
 
 enum ASSASSIN_STATE {
 	FlyToPlayer,
 	ChannelAttack,
 	FlyToPlanet,
+	GoIntoPlanet,
 	LurkOnPlanet,
 	Tumble,
 	AttackPlayer
 }
 
 var state = ASSASSIN_STATE.FlyToPlayer
+var last_state = null
 
 var health: float = 10.0
 var _target_player: Player = null
+var _target_planet: Planet = null
 var _velocity: Vector2 = Vector2.ZERO
 var _damage: float = 10.0
 var _channel_time = 0
 var _attack_velocity = Vector2.ZERO
+
+func state_to_str(s):
+	match s:
+		ASSASSIN_STATE.FlyToPlayer:
+			return 'FlyToPlayer'
+		ASSASSIN_STATE.ChannelAttack:
+			return 'ChannelAttack'
+		ASSASSIN_STATE.FlyToPlanet:
+			return 'FlyToPlanet'
+		ASSASSIN_STATE.GoIntoPlanet:
+			return 'GoIntoPlanet'
+		ASSASSIN_STATE.LurkOnPlanet:
+			return 'LurkOnPlanet'
+		ASSASSIN_STATE.Tumble:
+			return 'Tumble'
+		ASSASSIN_STATE.AttackPlayer:
+			return 'AttackPlayer'
+		var value:
+			return 'Unknown ' + str(value)
 
 func _ready() -> void:
 	_target_player = GameManager.players[randi() % GameManager.players.size()]
@@ -40,6 +67,13 @@ func _get_player_in_range():
 			return player
 	return null
 
+func _get_planet_in_range():
+	for planet in GameManager.planets:
+		var squared_distance = planet.position.distance_squared_to(position)
+		if squared_distance < SQUARED_PLANET_DISTANCE:
+			return planet
+	return null
+
 func _start_channel_attack(target_player):
 	_target_player = target_player
 	state = ASSASSIN_STATE.ChannelAttack
@@ -47,16 +81,33 @@ func _start_channel_attack(target_player):
 
 func _start_attack_player():
 	state = ASSASSIN_STATE.AttackPlayer
-	_attack_velocity = (_target_player.position - position) * ATTACK_SPEED
+	_attack_velocity = (_target_player.position - position).normalized() * ATTACK_SPEED
 	_channel_time = ATTACK_TIME
+
+func _start_fly_to_planet(target_planet):
+	state = ASSASSIN_STATE.FlyToPlanet
+	_target_planet = target_planet
+
+func _start_go_into_planet():
+	state = ASSASSIN_STATE.GoIntoPlanet
+	_channel_time = GO_INTO_PLANET_TIME
+	set_collision_mask_bit(0, false)
+
+func _start_lurk_on_planet():
+	state = ASSASSIN_STATE.LurkOnPlanet
+	_velocity = Vector2.ZERO
 
 func _process_fly_to_player():
 	look_at(_target_player.position)
 	_velocity += (_target_player.position - position).normalized() * SPEED
 
 	var player_in_range = _get_player_in_range()
-	if player_in_range != null:
+	if player_in_range:
 		_start_channel_attack(player_in_range)
+
+	var planet_in_range = _get_planet_in_range()
+	if planet_in_range:
+		_start_fly_to_planet(planet_in_range)
 
 func _process_channel_attack(delta: float):
 	look_at(_target_player.position)
@@ -65,7 +116,20 @@ func _process_channel_attack(delta: float):
 		_start_attack_player()
 
 func _process_fly_to_planet():
-	pass
+	look_at(_target_planet.position)
+	_velocity += (_target_planet.position - position).normalized() * SPEED
+
+func _process_go_into_planet(delta):
+	if _channel_time > 0:
+		_channel_time -= delta
+		_velocity = (_target_planet.position - position).normalized() * GO_INTO_PLANET_SPEED
+	else:
+		var target_point = _target_planet.position + (position - _target_planet.position).normalized() * _target_planet._radius
+		if (position - target_point).length_squared() < LURK_ON_PLANET_TARGET_POINT_DIFF:
+			_start_lurk_on_planet()
+		else:
+			_velocity = (target_point - position).normalized() * GO_INTO_PLANET_SPEED
+			look_at(target_point)
 
 func _process_lurk_on_planet():
 	pass
@@ -93,6 +157,9 @@ func _physics_process(delta: float) -> void:
 		ASSASSIN_STATE.FlyToPlanet:
 			_process_fly_to_planet()
 
+		ASSASSIN_STATE.GoIntoPlanet:
+			_process_go_into_planet(delta)
+
 		ASSASSIN_STATE.LurkOnPlanet:
 			_process_lurk_on_planet()
 			do_process_physics = false
@@ -103,15 +170,35 @@ func _physics_process(delta: float) -> void:
 		ASSASSIN_STATE.AttackPlayer:
 			_process_attack_player(delta)
 
-	if do_process_physics:
-		_process_physics(delta)
+	_process_movement(delta)
 
-func _process_physics(delta: float) -> void:
+	if last_state != state:
+		print('new state: ' + state_to_str(state))
+		last_state = state
+
+func _process_movement(delta: float) -> void:
 	var collision = move_and_collide(_velocity * delta)
 	if collision:
-		var collider = collision.collider
-		_velocity = _velocity.bounce(collision.normal)
-		if collider.is_in_group("Player") and collider.has_method("hit"):
-			collider.hit(_damage)
-			queue_free()
+		_process_collision(collision)
+
 	_velocity *= DRAG
+
+func _process_collision(collision):
+	var collider = collision.collider
+	if collider.is_in_group("Player") and collider.has_method("hit"):
+		collider.hit(_damage)
+		queue_free()
+
+	var do_bounce = true
+
+	match state:
+		ASSASSIN_STATE.FlyToPlanet:
+			if collider == _target_planet:
+				_start_go_into_planet()
+		ASSASSIN_STATE.GoIntoPlanet:
+			do_bounce = false
+
+	do_bounce = false
+	if do_bounce:
+		_velocity = _velocity.bounce(collision.normal)
+
--- gravityworrior/src/EnemySpawn.gd
+++ gravityworrior/src/EnemySpawn.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
 var _enemy_scene = preload("res://src/EnemyAssassin.tscn")
+var has_spawned = false
 
 func _ready() -> void:
 	$SpawnTimer.connect("timeout", self, "on_SpawnTimer_timeout")
 	
 func on_SpawnTimer_timeout() -> void:
+	if has_spawned:
+		return
+
 	if GameManager.current_game_state == GameManager.GameState.Fight:
-		for i in range (5):
+		for i in range (1):
 			$SpawnPath/SpawnPathLocation.set_offset(randi())
 			var enemy = _enemy_scene.instance()
 			add_child(enemy)
 			enemy.position = $SpawnPath/SpawnPathLocation.position
+		has_spawned = true
