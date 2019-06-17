9b01b593170de669d8c735f2f380ccac0eb857c1
Bluemi <bruno.schilling@protonmail.ch>
2019-06-18 00:49:58 +0200

    worked on destroyer
    - destroyer now circles the satellite, after attacking it
    - destroyer now avoids flying through planets

--- gravityworrior/src/Destroyer.gd
+++ gravityworrior/src/Destroyer.gd
@@ -1,19 +1,23 @@
 extends Node2D
 
 const SPEED = 2.0
-const FOLLOW_SPEED = 3.0
-const ATTACK_SPEED = 15.0
+const FOLLOW_SPEED = 4.0
+const ATTACK_SPEED = 20.0
 const DRAG = 0.95
 const MAX_ROTATION_ANGLE = 0.02
+const ROUTE_POINT_DISTANCE = 50
+const MIN_PLANET_ROUTE_DISTANCE = 55
+const CIRCLE_DISTANCE = 120
 
 const MAX_HEALTH = 50
 const FOLLOW_PROBABILITY: float = 0.33
 const SQUARED_ATTACK_RANGE = 20000
 const DAMAGE = 20
 
-const FOLLOW_PLAYER_TIME = 5
+const FOLLOW_PLAYER_TIME = 7
 const ATTACK_CHANNEL_TIME = 0.7
 const ATTACK_DURATION = 0.8
+const CIRCLE_DURATION = 15
 const DIE_TIME = 1.0
 
 enum DestroyerState {
@@ -30,6 +34,7 @@ var state
 var _velocity: Vector2 = Vector2.ZERO
 var _has_to_be_removed = false
 var _target_point: Vector2
+var _satellite_planet = null
 var _target_player = null
 var _channel_time = 0
 var _direction = null
@@ -50,6 +55,7 @@ func _on_hit(damage):
 func _ready():
 	$Body.connect("got_hit", self, "_on_hit")
 	_target_point = GameManager.satellite.position
+	_satellite_planet = _get_nearest_planet(_target_point)
 	_start_fly_to_sender()
 
 func _get_nearest_player():
@@ -62,6 +68,17 @@ func _get_nearest_player():
 			nearest_player_distance = player_distance
 
 	return nearest_player
+
+func _get_nearest_planet(pos):
+	var nearest_planet = null
+	var nearest_planet_distance = null
+	for planet in GameManager.planets:
+		var planet_distance = planet.position.distance_squared_to(position)
+		if nearest_planet == null or (planet_distance < nearest_planet_distance):
+			nearest_planet = planet
+			nearest_planet_distance = planet_distance
+
+	return nearest_planet
 
 func _sender_in_range():
 	return _target_point.distance_squared_to(position) < SQUARED_ATTACK_RANGE
@@ -85,6 +102,10 @@ func _start_attack():
 	state = DestroyerState.Attack
 	_channel_time = ATTACK_DURATION
 
+func _start_circle():
+	state = DestroyerState.CircleSender
+	_channel_time = CIRCLE_DURATION
+
 func _die():
 	state = DestroyerState.Dead
 	$Head.collision_layer = 0
@@ -97,15 +118,39 @@ func _die():
 		$"/root/Main/EnemySpawn".kill_count = kill_counter - 1
 	print(kill_counter)
 
+func _get_next_route_point(target_point):
+	var route_point: Vector2 = target_point
+	var index = 0
+	var route_points = []
+	while route_point.distance_to(position) > ROUTE_POINT_DISTANCE:
+		var route_point_to_position = (position - route_point).normalized() * ROUTE_POINT_DISTANCE
+		route_point += route_point_to_position
+
+		# press away from planet
+		var nearest_planet: Planet = _get_nearest_planet(route_point)
+		var planet_to_route_point: Vector2 = (route_point - nearest_planet.position)
+		var planet_route_distance = (nearest_planet.radius + MIN_PLANET_ROUTE_DISTANCE)
+		if planet_to_route_point.length() < planet_route_distance:
+			route_point = nearest_planet.position + planet_to_route_point.normalized() * planet_route_distance
+		route_points.append(route_point)
+
+		index += 1
+		if index > 25:
+			break
+
+	return route_point
+
 func _process_fly_to_sender():
-	_velocity += (_target_point - position).normalized() * SPEED
+	var route_point = _get_next_route_point(_target_point)
+	_velocity += (route_point - position).normalized() * SPEED
 	_velocity *= DRAG
 
 	if _sender_in_range():
 		_start_channel_attack()
 
 func _process_follow_player(delta):
-	_velocity += (_target_player.position - position).normalized() * FOLLOW_SPEED
+	var route_point = _get_next_route_point(_target_player.position)
+	_velocity += (route_point - position).normalized() * FOLLOW_SPEED
 	_velocity *= DRAG
 
 	_channel_time -= delta
@@ -123,10 +168,22 @@ func _process_attack(delta):
 	_channel_time -= delta
 	if _channel_time < 0:
 		GameManager.satellite.hit(DAMAGE)
-		_start_follow_player()
+		_start_circle()
 
 	_velocity += (_target_point - position).normalized() * ATTACK_SPEED
 	_velocity *= DRAG
+
+func _process_circle_sender(delta):
+	var planet_to_position = (position - _satellite_planet.position).normalized() * CIRCLE_DISTANCE
+	var movement_vec = planet_to_position.rotated(-PI/2).normalized() * 10
+	var target_point = _satellite_planet.position + planet_to_position + movement_vec
+
+	_velocity += (target_point - position).normalized() * SPEED
+	_velocity *= DRAG
+
+	_channel_time -= delta
+	if _channel_time <= 0:
+		_start_fly_to_sender()
 
 func _process_dead(delta):
 	_velocity *= DRAG
@@ -157,7 +214,7 @@ func _physics_process(delta: float) -> void:
 		DestroyerState.FollowPlayer:
 			_process_follow_player(delta)
 		DestroyerState.CircleSender:
-			pass
+			_process_circle_sender(delta)
 		DestroyerState.Dead:
 			_process_dead(delta)
 
