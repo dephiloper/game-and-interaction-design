3a5ed5dae32800638296cfaefe68db172350284b
Bluemi <bruno.schilling@protonmail.ch>
2019-06-23 13:52:19 +0200

    fixed buff selection position

--- gravityworrior/src/UILayer.gd
+++ gravityworrior/src/UILayer.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 const BUFF_SELECTION_SCENE = preload("res://src/BuffSelection.tscn")
+const BUFF_SECECTION_OFFSET = Vector2(-35, -32) # idk, magic number
 
 func reinstantiate_buff_selection():
 	var buff_scene = BUFF_SELECTION_SCENE.instance()
 	add_child(buff_scene)
 	buff_scene.name = "BuffSelection"
-	buff_scene.position = get_viewport().size / 2
+	
+	var mid = get_viewport().size / 2 + BUFF_SECECTION_OFFSET
+	buff_scene.margin_left = mid.x - (buff_scene.get_node("ColorRect").rect_size.x/2)
+	buff_scene.margin_top = mid.y - (buff_scene.get_node("ColorRect").rect_size.y/2)
