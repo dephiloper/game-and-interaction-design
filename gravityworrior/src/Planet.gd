fb69cf61071757495704f0ca318b51759469e62f
Philipp <philippboensch@gmail.com>
2019-07-03 23:58:06 +0200

    hit feedback for aliens implemented

--- gravityworrior/src/Planet.gd
+++ gravityworrior/src/Planet.gd
@@ -4,9 +4,9 @@ class_name Planet
 
 const GRAVITY_EXPONENT: float = 2.95
 
-var gravity: float = 0.0
 var radius: float = 0.0
 
+var gravity: float = 0.0
 
 var _planet_points: PoolVector2Array
 var _num_points: int = 0.0
@@ -27,7 +27,7 @@ func _draw() -> void:
 	if self._initialized:
 		_draw_planet(self._planet_points)
 
-func generate(center: Vector2, r: float = rand_range(21, 42), 
+func generate(center: Vector2, r: float = rand_range(24, 42), 
 	num_points: int = int(rand_range(12,20)), 
 	offset: float = rand_range(2,4)) -> void:
 	self.position = center
--- gravityworrior/src/Satellite.gd
+++ gravityworrior/src/Satellite.gd
@@ -4,22 +4,26 @@ var HealthBarScene = preload("res://src/HealthBar.tscn")
 
 class_name Satellite
 
-const SATELLITE_IN_PLANET_Y_OFFSET = 1.5
+const SATELLITE_IN_PLANET_Y_OFFSET = 1.7
 
 var health: float = 100
 var max_health: float = 100
 var _health_bar
 var _heal_radius: float
+var _pulsating_radius: float
 var _closest_planet: Planet
 var _life_regeneration_value: float = 0.1
 var _heal_area_position: Vector2
 var _containing_players: Array = []
+var _elapsed_time: float
+var _life_regeneration_color: Color = Color(0, 0.67, 0.4, 0.25)
 
 signal game_over
 
 func _init() -> void:
+	add_to_group("Satellite")
 	GameManager.set_satellite(self)
-
+	
 func is_dead():
 	return health <= 0
 
@@ -47,7 +51,12 @@ func _ready() -> void:
 	$HealArea.connect("body_entered", self, "_on_HealArea_body_entered")
 	$HealArea.connect("body_exited", self, "_on_HealArea_body_exited")
 	
-func hit(damage: float) -> void:
+func _process(delta: float) -> void:
+	_elapsed_time += delta
+	_pulsating_radius = sin(_elapsed_time) * _heal_radius
+	update()
+
+func damage(damage: float) -> void:
 	health = max(health - damage, 0)
 	if health <= 0:
 		emit_signal("game_over")
@@ -64,7 +73,9 @@ func get_closest_planet() -> Planet:
 	return closest_planet
 	
 func _draw() -> void:
-	draw_circle(_heal_area_position, _heal_radius, Color(0, 1, 0, 0.3))
+	draw_circle(_heal_area_position, _heal_radius, _life_regeneration_color)
+	draw_circle(_heal_area_position, _pulsating_radius, _life_regeneration_color)
+	
 
 func _on_HealArea_body_entered(body: PhysicsBody2D) -> void:
 	if body.is_in_group("Player"):
