d1284502c35df45cc70dde1c01e7a968182b97b7
Philipp <philippboensch@gmail.com>
2019-07-11 11:22:46 +0200

    laser pointer with toggle functionality added

--- gravityworrior/src/Gun.gd
+++ gravityworrior/src/Gun.gd
@@ -2,12 +2,9 @@ extends Node2D
 
 class_name Gun
 
-enum TYPE {
-	MACHINE, GATLING, RIFLE, LAUNCHER
-}
-
 const BULLET_SCENE = preload("res://src/Bullet.tscn")
 const CROSS_HAIR_DISTANCE: int = 128
+const GUN_POINTER_COLOR: Color = Color(1, 0, 0, 0.25)
 
 var shoot_dir = Vector2.RIGHT
 var can_shoot: bool = true
@@ -16,6 +13,7 @@ var _fire_rate: float = 5
 var _controls: Controls
 var _offset: Vector2 = Vector2(4,2)
 var _alternative_aiming_enabled: bool = false
+var _show_laser_pointer: bool = true
 
 func set_controls(controls: Controls) -> void:
 	_controls = controls
@@ -35,6 +33,9 @@ func _physics_process(delta: float) -> void:
 	if GameManager.current_game_state != GameManager.GameState.Fight:
 		return
 	
+	if _controls.just_pressed("toggle_laser_pointer"):
+		_show_laser_pointer = !_show_laser_pointer
+	
 	#if _controls.just_pressed("toggle_alternative_aiming"):
 		#print("_alternative_aiming_enabled",_alternative_aiming_enabled)
 		#_alternative_aiming_enabled = !_alternative_aiming_enabled
@@ -46,8 +47,12 @@ func _physics_process(delta: float) -> void:
 	$GunSprite.set_flip_v(shoot_dir.x < 0)
 	$GunSprite/BarrelPosition.position.y = 10 if (shoot_dir.x < 0) else -10 
 	position = _offset if shoot_dir.x >= 0 else Vector2(-_offset.x, _offset.y)
-	
-		
+	update()
+
+func _draw() -> void:
+	if _show_laser_pointer:
+		draw_line($GunSprite.position, $CrosshairSprite.position, GUN_POINTER_COLOR, 2)
+
 func _caculate_cross_hair_direction() -> Vector2:
 	var horizontal: float = 0
 	var vertical: float = 0
